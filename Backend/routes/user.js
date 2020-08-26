27f6706ad4560443ca960f92972efd6f4be409f1
Gabriel <dreadpool38@gmail.com>
2020-08-26 00:06:19 -0300

    token

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { createUser, loginUser, addSubscribe } = require('../controller/user');
+const UserCOntroller = require('../controller/user')
 const validateFields = require('../middlewares/validateFields');
 const { TYPE_VALIDATE_LOGIN, TYPE_VALIDATE_CREATE_USER, TYPE_VALIDATE_ADDRESS } = require('../services/enum/validate');
 const validateToken = require('../middlewares/validateToken');
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post('/login', validateFields(TYPE_VALIDATE_LOGIN), loginUser);
 router.post('/create', validateFields(TYPE_VALIDATE_CREATE_USER), createUser);
 router.post('/subscribe', validateToken, validateFields(TYPE_VALIDATE_ADDRESS), addSubscribe);
+router.post('/verify', UserCOntroller.verifyToken);
 
 module.exports = router;
